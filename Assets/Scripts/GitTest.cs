eec6ef1b039f8b510068193d6430ecd28b220cb1
Unicorn-web <58799490+Unicorn-web@users.noreply.github.com>
2024-04-23 17:29:17 +0800

    add GitTest code

---
+++ Assets/Scripts/GitTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    // 实现最终的定位效果
+
+    private void Awake()
+    {
+        Debug.Log("新增验证git是否有效");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
